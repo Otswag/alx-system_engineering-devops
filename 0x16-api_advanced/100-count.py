a1a9841c8aeffdb0b8a427bcaa5985bbeafdc32d
Otswag <106761383+Otswag@users.noreply.github.com>
2023-03-17 01:13:30 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,44 +1,53 @@
 #!/usr/bin/python3
 import requests
 
-def count_words(subreddit, word_list, after=None, counts={}):
-    """ Base case: no more posts to fetch """
-    if after == '':
-        """ Sort the counts dictionary by count (descending) and word (ascending) """
-        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-        """ Print the counts for each keyword """
-        for word, count in sorted_counts:
-            print(word + ':', count)
+def count_words(subreddit, word_list, after=None, word_count={}):
+    """Recursively queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords.
+    
+    Args:
+    subreddit (str): The name of the subreddit to search.
+    word_list (list): A list of keywords to count.
+    after (str): The token of the next page of results to fetch.
+    word_count (dict): A dictionary to store the counts of each keyword.
+    
+    Returns:
+    None
+    """
+    # Base case: If all pages have been fetched, print the sorted counts of keywords.
+    if after == 'STOP':
+        counts = [(k, v) for k, v in word_count.items()]
+        counts.sort(key=lambda x: (-x[1], x[0]))
+        for word, count in counts:
+            print(f"{word}: {count}")
         return
-
-    """ Fetch the next page of posts from the subreddit """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    params = {'limit': 100, 'after': after}
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    # Build the API request URL.
+    base_url = "https://www.reddit.com"
+    endpoint = f"/r/{subreddit}/hot.json"
+    params = {"limit": 100}
+    if after:
+        params["after"] = after
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = base_url + endpoint
+    
+    # Send the API request.
     response = requests.get(url, params=params, headers=headers)
-
-    """ Check for a valid response """
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print(f"Error {response.status_code}: Could not fetch data from {url}")
         return
-
-    """ Parse the response JSON """
+    
+    # Parse the response JSON and extract the titles.
     data = response.json()
-    posts = data['data']['children']
-
-    """ Process each post """
-    for post in posts:
-        title = post['data']['title'].lower()
-        """ Count the occurrences of each keyword in the title """
-        for word in word_list:
-            count = title.count(word.lower())
-            if count > 0:
-                """ Add the count to the counts dictionary """
-                if word in counts:
-                    counts[word] += count
-                else:
-                    counts[word] = count
-
-    """ Recursively call the function with the after parameter set to the last post's ID """
-    last_post_id = posts[-1]['data']['name']
-    count_words(subreddit, word_list, after=last_post_id, counts=counts)
+    titles = [post["data"]["title"] for post in data["data"]["children"]]
+    
+    # Count the occurrences of each keyword in the titles.
+    for title in titles:
+        words = title.lower().split()
+        for word in words:
+            word = word.strip(".,?!-_:;")
+            if word in word_list:
+                word_count[word] = word_count.get(word, 0) + 1
+    
+    # Recursively call the function with the token of the next page of results.
+    next_page = data["data"].get("after")
+    count_words(subreddit, word_list, next_page, word_count)
